f9f4ed64c7e394bf39e882c80cbbcce1f75944f9
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-10-18 01:22:03 +0300

    DONE

--- lib/a_name_error.rb
+++ lib/a_name_error.rb
@@ -1,3 +1,12 @@
+#puts "hello world"
 "hello world"
 
-hello_world
+"hello world"
+
+#FOUR TYPES OF ERRORS
+#NAME -either invalid or undefined
+#SYNTAX -Incorrect syntax
+#times do 
+ #   puts "hi"
+#TYPE -Occurs when you want to do a maths opration of two different objects i.e an int + string
+#1 + "1"
